94c9332e9597068e6939fdeff2853c65fcad69f7
Mamun-swe <mhmamun166009@gmail.com>
2022-08-06 16:24:07 +0600

    Remote origin changes

--- components/head/index.js
+++ components/head/index.js
@@ -11,11 +11,11 @@ export const MainHead = () => {
             <meta name="robots" content="index, follow" />
             <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
             <meta name="language" content="English" />
-            <meta name="twitter:image" content="https://researchportal-xyz.vercel.app/static/meta-image.png" />
-            <meta property="og:site_name" content="Eazybest Limited" />
-            <meta property="og:image" content="https://researchportal-xyz.vercel.app/static/meta-image.png" />
-            <meta property="og:image:alt" content="https://researchportal-xyz.vercel.app/static/meta-image.png" />
-            <meta property="og:image:secure_url" content="https://researchportal-xyz.vercel.app/static/meta-image.png" />
+            <meta name="twitter:image" content="https://aurlab.vercel.app/static/meta-image.png" />
+            <meta property="og:site_name" content="AurLab" />
+            <meta property="og:image" content="https://aurlab.vercel.app/static/meta-image.png" />
+            <meta property="og:image:alt" content="https://aurlab.vercel.app/static/meta-image.png" />
+            <meta property="og:image:secure_url" content="https://aurlab.vercel.app/static/meta-image.png" />
             <meta property="og:image:type" content="image/jpeg" />
             <meta property="og:image:width" content="500" />
             <meta property="og:image:height" content="500" />
