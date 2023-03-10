498d340c78acb25ab5a707842303f3b7dd3dc013
Mamun-swe <mamun.swe.277@gmail.com>
2023-03-10 19:11:44 +0600

    API endpoint changes

--- pages/api/axios.config.js
+++ pages/api/axios.config.js
@@ -1,43 +1,37 @@
+import axios from "axios";
 
-import axios, { AxiosRequestConfig } from "axios"
-
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL
-const API_KEY = process.env.NEXT_PUBLIC_API_KEY
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const accessToken = () => {
-    if (typeof window !== 'undefined') {
-        return localStorage.getItem("token")
-    } else {
-        return null
-    }
-}
+  if (typeof window !== "undefined") {
+    return localStorage.getItem("token");
+  } else {
+    return null;
+  }
+};
 
 // common config
-axios.defaults.headers.post["Content-Type"] = "application/json"
-axios.defaults.headers.common['api_key'] = API_KEY
+axios.defaults.headers.post["Content-Type"] = "application/json";
 
-const publicRequest = axios.create({ baseURL: BASE_URL })
+const publicRequest = axios.create({ baseURL: BASE_URL });
 
-const privateRequest = axios.create({ baseURL: BASE_URL })
+const privateRequest = axios.create({ baseURL: BASE_URL });
 
 // Add a request interceptor
 privateRequest.interceptors.request.use(
-    async (config) => {
-        if (config.headers === undefined) {
-            config.headers = {};
-        }
-        if (accessToken) {
-            config.headers["Authorization"] = "Bearer " + accessToken() || "";
-        }
-        return config;
-    },
-    (err) => {
-        console.log(err);
-        Promise.reject(err);
+  async (config) => {
+    if (config.headers === undefined) {
+      config.headers = {};
     }
+    if (accessToken) {
+      config.headers["Authorization"] = "Bearer " + accessToken() || "";
+    }
+    return config;
+  },
+  (err) => {
+    console.log(err);
+    Promise.reject(err);
+  }
 );
 
-export {
-    publicRequest,
-    privateRequest
-}
+export { publicRequest, privateRequest };
