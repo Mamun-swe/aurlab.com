ec475afb8189c48a6d45bf3bce94f42ebb89e1fb
mamun-swe <mamun.swe.277@gmail.com>
2023-05-07 23:12:33 +0600

    Tawk chat API integrated

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,11 @@
-import '../styles/globals.css'
-import NextNprogress from 'nextjs-progressbar'
-import { MainHead } from '../components/head'
+import "../styles/globals.css";
+import NextNprogress from "nextjs-progressbar";
+import TawkMessengerReact from "@tawk.to/tawk-messenger-react";
+import { MainHead } from "../components/head";
 
 function MyApp({ Component, pageProps }) {
+  const TAWK_PROPERTY_ID = process.env.NEXT_PUBLIC_TAWK_PROPERTY_ID;
+  const TAWK_WIDGET_ID = process.env.NEXT_PUBLIC_TAWK_WIDGET_ID;
   return (
     <>
       <MainHead />
@@ -13,9 +16,13 @@ function MyApp({ Component, pageProps }) {
         height={2}
         options={{ showSpinner: false }}
       />
+      <TawkMessengerReact
+        propertyId={TAWK_PROPERTY_ID}
+        widgetId={TAWK_WIDGET_ID}
+      />
       <Component {...pageProps} />
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
